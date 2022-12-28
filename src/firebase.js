61b789fd3f50f9dbcadc982041b9a09399057642
Mateus Cardoso <serjumano17@gmail.com>
2022-12-28 20:52:21 -0300

    feat: :sparkles: add .env file to put env variables to project

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWKWvCcPHPLhXpEWsEi9PwzqZW3JLzP38",
-  authDomain: "linkedin-clone-8c8d2.firebaseapp.com",
-  projectId: "linkedin-clone-8c8d2",
-  storageBucket: "linkedin-clone-8c8d2.appspot.com",
-  messagingSenderId: "778445800498",
-  appId: "1:778445800498:web:8c017a17adf1bd1d33282b",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
